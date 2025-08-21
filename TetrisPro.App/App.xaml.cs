e15f6c5650cb7d6acf8587222ea4eae8e98bfbfc
alexgold25 <alexandrgoldenbergg@gmail.com>
2025-08-21 05:55:34 +0500

    Add project files.

---
+++ TetrisPro.App/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TetrisPro.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
