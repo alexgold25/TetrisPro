ac1469c5851a4dd354110925844ec39f100f0b4a
alexgold25 <alexandrgoldenbergg@gmail.com>
2025-08-21 15:04:30 +0500

    pre test

---
+++ src/TetrisPro.App/Controls/PlayfieldControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TetrisPro.App.Controls
+{
+    public partial class PlayfieldControl : UserControl
+    {
+        public PlayfieldControl() => InitializeComponent();
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(
+                nameof(ItemsSource),
+                typeof(IEnumerable),
+                typeof(PlayfieldControl),
+                new PropertyMetadata(null));
+
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+    }
+}
